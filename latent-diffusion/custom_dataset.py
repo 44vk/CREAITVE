1e1c365fecfd08b7564608e9a760c95c977b8d35
Valentine Kazmirchuk <valentine@Valentines-MacBook-Air.local>
2024-10-14 13:39:56 +0100

    Remove submodules and add diffusers and latent-diffusion as regular folders

---
+++ latent-diffusion/custom_dataset.py
@@ -0,0 +1,20 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+class CustomImageDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.image_files = [f for f in os.listdir(root_dir) if f.endswith(('png', 'jpg', 'jpeg'))]
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root_dir, self.image_files[idx])
+        image = Image.open(img_path).convert('RGB')  # Convert to RGB to ensure consistency
+        if self.transform:
+            image = self.transform(image)
+        return image
